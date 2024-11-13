57a6a57abbc54a83459b82d5bf78ba6120a218b7
gabrielmrib7 <gabrielmrib@hotmail.com>
2024-11-13 04:25:19 -0300

    Alguns componentes e estilizações feitos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,19 @@
-
-
-
+import './styles.css';
+import { Header } from "../../components/Header";
+import background from '../../assets/background.png'
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app"/>
+        <div className="info">
+          <div >
+              <input name="usuario" placeholder="@usuario"/>
+              <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
