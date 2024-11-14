e9510bb01b315275bf37e912c3bfcfbdf4d35bd5
gabrielmrib7 <gabrielmrib@hotmail.com>
2024-11-14 04:15:57 -0300

    Ajustes Finais

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,9 +12,13 @@ function App() {
   const handleGetData = async () => {
     const userData = await fetch(`https://api.github.com/users/${user}`);
     const newUser = await userData.json();
+    console.log(newUser);
 
-    if(newUser.name)
+    if(newUser.login)
     {
+      var erro = document.getElementById('erro');
+      erro.style.visibility = 'hidden';
+      erro.style.padding = '0';
       const {avatar_url, name, bio, login} = newUser;
       setCurrentUser({avatar_url, name, bio, login});
       const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
@@ -23,24 +27,39 @@ function App() {
       if(newRepos.length)
       {
         setRepos(newRepos);
-        console.log(newRepos);
         
       }
 
     }
+    else
+    {
+      var erro = document.getElementById('erro');
+      erro.innerText = newUser.message;
+      erro.style.visibility = 'visible';
+      erro.style.padding = '.75rem 1.25rem';
+    }
+    
   }
 
   return (
     <div className="App">
       <Header />
+      <div class="alert alert-dark m-2" id="erro" role="alert">
+               
+      </div>
       <div className="conteudo">
         <img src={background} className="background" alt="background app"/>
         <div className="info">
-          <div >
+          <div className="buscar">
               <input name="usuario" placeholder="@usuario" value={user} onChange={event => setUser(event.target.value)}/>
-              <button onClick={handleGetData}>Buscar</button>
+              <button class="desktopButton" onClick={handleGetData}>Buscar</button>
+              <button className='mobileButton' onClick={handleGetData}><svg class="svg-icon search-icon" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">
+                A magnifying glass icon.</desc><g class="search-path" fill="none" stroke="#848F91">
+                  <path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg></button>
+        
           </div>
-          {currentUser?.name ? ( <>
+          {currentUser?.login ? ( <>
               <div className="perfil">
               <img src={currentUser.avatar_url} className="profile" alt="perfil"></img>
               <div>
@@ -56,12 +75,12 @@ function App() {
           <div className="repositorios">
             <h4>Repositórios</h4>
             {repos.map(repo => (
-              <a href={repo.html_url}>
+              <a href={repo.html_url} target="_blank">
               <ItemList title={repo.name} description={repo.description}/>            
               </a>
             ))}
           </div>
-              ):null}
+              ): null}
         </div>
       </div>
     </div>
